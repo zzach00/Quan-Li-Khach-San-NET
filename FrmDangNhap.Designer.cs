876e050d3f135ad50c51df854ee5b6b94dbf9766
Zach00 <hieptrong790@gmail.com>
2021-10-21 23:48:57 +0700

    Đã thêm form đăng nhập

---
+++ FrmDangNhap.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace Quan_Li_Khach_San_NET
+{
+    partial class FrmDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTenDangNhap = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.btmTimKiemThongTin = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(410, 67);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(213, 44);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Đăng nhập";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(170, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 29);
+            this.label1.TabIndex = 41;
+            this.label1.Text = "Tên đăng nhập";
+            // 
+            // txtTenDangNhap
+            // 
+            this.txtTenDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenDangNhap.Location = new System.Drawing.Point(359, 155);
+            this.txtTenDangNhap.Name = "txtTenDangNhap";
+            this.txtTenDangNhap.Size = new System.Drawing.Size(297, 34);
+            this.txtTenDangNhap.TabIndex = 40;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(170, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 29);
+            this.label2.TabIndex = 41;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMatKhau.Location = new System.Drawing.Point(359, 237);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(297, 34);
+            this.txtMatKhau.TabIndex = 42;
+            // 
+            // btmTimKiemThongTin
+            // 
+            this.btmTimKiemThongTin.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btmTimKiemThongTin.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btmTimKiemThongTin.Location = new System.Drawing.Point(719, 148);
+            this.btmTimKiemThongTin.Name = "btmTimKiemThongTin";
+            this.btmTimKiemThongTin.Size = new System.Drawing.Size(163, 49);
+            this.btmTimKiemThongTin.TabIndex = 85;
+            this.btmTimKiemThongTin.Text = "Đăng nhập";
+            this.btmTimKiemThongTin.UseVisualStyleBackColor = false;
+            this.btmTimKiemThongTin.Click += new System.EventHandler(this.btmTimKiemThongTin_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(719, 225);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(163, 52);
+            this.btnThoat.TabIndex = 86;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // FrmDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 413);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btmTimKiemThongTin);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTenDangNhap);
+            this.Controls.Add(this.label6);
+            this.Name = "FrmDangNhap";
+            this.Text = "FrmDangNhap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTenDangNhap;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.Button btmTimKiemThongTin;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
---
+++ FrmDangNhap.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Quan_Li_Khach_San_NET
+{
+    public partial class FrmDangNhap : Form
+    {
+        public FrmDangNhap()
+        {
+            InitializeComponent();
+        }
+
+        Ketnoi kn = new Ketnoi();
+        
+        private void btmTimKiemThongTin_Click(object sender, EventArgs e)
+        {
+            kn.KetNoi_Dulieu();
+
+            string Acc = txtTenDangNhap.Text;
+            string Pw = txtMatKhau.Text;
+            string sql_Login = "Select * from quyennv where id = '" + Acc + "' and matkhau = '" + Pw + "'";
+
+            SqlCommand cmd = new SqlCommand(sql_Login, kn.cnn);
+            SqlDataReader dataRead = cmd.ExecuteReader();
+
+            if (dataRead.Read() == true)
+            {
+                MessageBox.Show("Ban da dang nhap thanh cong");
+                Form frmain = new FrmHome();
+                frmain.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Dang nhap lai");
+                txtTenDangNhap.Focus();
+            }
+
+        }
+    }
+}
